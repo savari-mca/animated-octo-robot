ce331e2dda00649e2e278baec3062747690fd03f
savari-mca <savari.mca@gmail.com>
2026-03-04 22:40:29 +0530

    envirnement setup

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 def main():
-    print("Hello from langchain-course!")
+    print(os.getenv("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
